e8acd76dc48762669187ad02efc43526206a4206
EdsonRosales97 <erosales@applab.mx>
2022-05-24 18:45:30 -0500

    Iterations to be able to position the elements in the grid and show their status through a color

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
+import Cell from './Cell'
 
 /**
  * 
  * @returns The plane drawn with the points where the cells will inhabit
  */
-const Grid = () => {
+const Grid = ({ grid }) => {
   return (
     <main>
-
+        {/* Iteration of the main grid, each grid returns a row */}
+        {
+            grid.map(row => <div>
+                {/* Each row will iterate to generate the element and return a column */}
+                {
+                    row.map(value => 
+                        <Cell alive={value} />
+                    )
+                }
+            </div>)
+        }
     </main>
   )
 }
